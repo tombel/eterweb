acba2d3cc53b57675ed74ee2d78ed5fb9b6c9218
tombel <tomasbelardo@gmail.com>
2023-04-20 15:00:03 -0300

    social icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,16 @@
+import logo1 from "../assets/images/instagram.png"
+import logo2 from "../assets/images/twitter.png"
+import logo3 from "../assets/images/linkedin.png"
+
 function Contact() {
     return ( 
         <div className="contact">
             <div >
                 <p className='text'>CONTACT</p>
-                <p className="experience">INSTAGRAM</p>
-                <p className="experience">TWITTER</p>
-                <p className="experience">LINKEDIN</p>
+<a className="navbar-logo" onClick={() => handleLogoClick()}><img src={logo1} className="logo" alt="ETER logo"/></a>
+<a className="navbar-logo" onClick={() => handleLogoClick()}><img src={logo2} className="logo" alt="ETER logo"/></a>
+<a className="navbar-logo" onClick={() => handleLogoClick()}><img src={logo3} className="logo" alt="ETER logo"/></a>
+                <p className="creative">eter@eter.studio</p>
             </div>
         </div>
      );
