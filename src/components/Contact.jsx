85878ec3d22e7a3e7f7eb889fb12db56d8e01c59
tombel <tomasbelardo@gmail.com>
2023-04-20 15:19:54 -0300

    link to socials

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,9 +7,9 @@ function Contact() {
         <div className="contact">
             <div >
                 <p className='text'>CONTACT</p>
-<a className="navbar-logo" onClick={() => handleLogoClick()}><img src={logo1} className="logo" alt="ETER logo"/></a>
-<a className="navbar-logo" onClick={() => handleLogoClick()}><img src={logo2} className="logo" alt="ETER logo"/></a>
-<a className="navbar-logo" onClick={() => handleLogoClick()}><img src={logo3} className="logo" alt="ETER logo"/></a>
+<a href ="https://www.instagram.com/eter__studio/" className="navbar-logo"><img src={logo1} className="logo" alt="ETER logo"/></a>
+<a href="https://twitter.com/eter__studio"  className="navbar-logo"><img src={logo2} className="logo" alt="ETER logo"/></a>
+<a  href="https://www.linkedin.com/company/eter-studio/" className="navbar-logo"><img src={logo3} className="logo" alt="ETER logo"/></a>
                 <p className="creative">eter@eter.studio</p>
             </div>
         </div>
